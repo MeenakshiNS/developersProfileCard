0b824ccd855b7fa9876000ce9ffeade5b6995b25
Meenakshi <hiringhrbot39@gmail.com>
2023-10-03 01:29:00 +0530

    first

--- src/index.js
+++ src/index.js
@@ -1,17 +1,71 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <Details />
+      <Skillist />
+    </div>
+  );
+}
+
+function Avatar() {
+  return (
+    <div className="avatar">
+      <h1>DEVELOPERS PROFILE CARD</h1>
+      <img src="/money..png" alt="" />
+    </div>
+  );
+}
+function Details() {
+  return (
+    <div className="data">
+      <h2> N S Meenakshi</h2>
+      <p>
+        Full stack MERN developer.Currently working in
+        Brototype,trivandrum,kerala.Now I have 1 year experience .I also do freelancing.My charge for creating a fully functional website is 50k and above.Dont miss the chance to procure your own website
+      </p>
+      <span>---------------------------------- </span>
+      <span>gitHub:https://github.com/MeenakshiNS</span>
+    </div>
+  );
+}
+
+function Skillist() {
+  return (
+    <div className="skill-list">
+      <Skills />
+      <Skills skill="HTML,CSS" emoji="🫰" color="#7B876D" />
+      <Skills skill="react" color="#893F04" emoji="👶" />
+      <Skills skill="redux" color="#D49B7E" emoji="😌 " />
+      <Skills skill="javascript" color="#C67F43" emoji="🥳 " />
+      <Skills skill="git and git hub" color="#C67F43" emoji="🫡" />
+      <Skills skill="Nodejs" color="#C67F43" emoji="🤓" />
+      <Skills skill="Mongodb" color="#C67F43" emoji="😎" />
+    </div>
+  );
+}
+
+function Skills(props) {
+  console.log(props);
+  return (
+    <div className="skill">
+      <span style={{ backgroundColor: props.color }}>
+        <h3>
+          {props.skill}
+          {props.emoji}
+        </h3>
+      </span>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
